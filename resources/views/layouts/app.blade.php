0824cf9b31c5d025d14fb6d6fe4e831d6147726d
denliedesign <customdenlie@gmail.com>
2023-11-13 09:51:27 -0600

    turn off vite app.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
         <link rel="stylesheet" href="/css/style.css">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+{{--        @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
