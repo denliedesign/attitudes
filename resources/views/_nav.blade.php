dd324acbe794ba9066a290fe94661ae52fdc571a
denliedesign <customdenlie@gmail.com>
2023-11-22 14:57:35 -0600

    modify nav mobile width 100 attempt 2

--- resources/views/_nav.blade.php
+++ resources/views/_nav.blade.php
@@ -34,14 +34,14 @@
 </div>
 
 <div class="d-block d-lg-none stix-mobile" style="position: fixed; width: 100%; z-index: 5;">
-    <div class="accordion" id="accordionExample">
+    <div class="accordion" id="accordionExample" style="width: 100%;">
         <div class="accordion-item" style="border: none !important;">
             <h2 class="accordion-header d-flex justify-content-between" id="headingOne">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                     <ion-icon name="menu-outline" style="font-size: 1.5em; border: 1px solid lightgray; color: #7F7F7F;" class="m-3 px-2 rounded"></ion-icon>
                 </button>
                 <div class="d-flex align-items-center">
-                    <a class="navbar-brand d-lg-none" href="/"><img src="/images/logo-aim.png" alt="" style="max-width: 133px;"></a>
+                    <a class="navbar-brand d-lg-none" href="/"><img src="/images/logo-aim.png" alt="" style="max-width: 133px; height: auto;"></a>
                 </div>
             </h2>
             <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
@@ -57,7 +57,7 @@
 <div style="height: 92.29px;"></div>
 
 <div class="d-none d-lg-block">
-    <nav class="navbar navbar-expand-lg bg-white font-syne fixed-top stix py-0" style="top: 43.6px; height: 108.29px; width: 100%;">
+    <nav class="navbar navbar-expand-lg bg-white font-syne fixed-top stix py-0" style="top: 43.6px; height: 108.29px;">
         <div class="container-fluid">
 {{--            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">--}}
 {{--                <span class="navbar-toggler-icon"></span>--}}
