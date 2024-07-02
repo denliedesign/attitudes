151e9d9a929ba7616eb03e02f9f095c27554f424
denliedesign <customdenlie@gmail.com>
2024-07-01 20:12:33 -0500

    childrens dance classes summer canva preview

---
+++ resources/views/childrens-dance-classes-summer-2024.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="banner-wrap d-block" style="position: relative;">
+        <div class="banner"></div>
+        <div class="custom-shape-divider-bottom-1663856745">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" class="shape-fill"></path>
+            </svg>
+        </div>
+    </div>
+
+    <div class="container">
+        <div style="position: relative; width: 100%; height: 0; padding-top: 77.2727%;
+ padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+ border-radius: 8px; will-change: transform;">
+            <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+                    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGFTxabuBY&#x2F;Ay5DKD5bOTAiT6LVvFM8zg&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+            </iframe>
+        </div>
+    </div>
+{{--    <div class="bg-white">--}}
+{{--        <div class="container pb-5 pt-3">--}}
+{{--            <div class="my-4 row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">--}}
+{{--                <div class="col d-flex align-items-center">--}}
+{{--                    <div class="text-end">--}}
+{{--                        <h3 class="font-staat" style="font-size: 100px; line-height: 0.9em;">{{ $head1 }}</h3>--}}
+{{--                        <p class="font-syne" style="font-size: 22px;">--}}
+{{--                            {{$content1}}--}}
+{{--                        </p>--}}
+{{--                        <a href="/schedule" class="text-decoration-none"><div class="btn btn-family rounded btn-green shadow mx-2">GET STARTED</div></a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+@endsection
+
