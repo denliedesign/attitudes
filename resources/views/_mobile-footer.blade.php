07796c6808fb05b7fd7a1481ba6401d8f5ada547
denliedesign <customdenlie@gmail.com>
2024-07-01 21:00:06 -0500

    no margins or paddings for sticky foot

--- resources/views/_mobile-footer.blade.php
+++ resources/views/_mobile-footer.blade.php
@@ -1,5 +1,5 @@
-<div class="d-block d-md-none">
-    <div class="row d-flex justify-content-center py-2" style="background: white; z-index: 100; position: fixed; bottom: 0; width: 100vw; min-width: 100%; box-shadow: 0 -4px 10px -4px rgba(0, 0, 0, 0.5);">
+<div class="d-block d-md-none px-0 mx-0">
+    <div class="row d-flex justify-content-center py-2 px-0 mx-0" style="background: white; z-index: 100; position: fixed; bottom: 0; width: 100vw; min-width: 100%; box-shadow: 0 -4px 10px -4px rgba(0, 0, 0, 0.5);">
         <div class="col text-center"><a style="text-decoration: none; color: black;" href="tel:516-756-5144"><ion-icon style="color: #AFD237; font-size: 22px;" name="call"></ion-icon><br>Phone</a></div>
         <div class="col text-center"><a style="text-decoration: none; color: black;" href="mailto:info@attitudesmotion.com"><ion-icon style="color: #E2459C; font-size: 22px;" name="mail"></ion-icon><br>Email</a></div>
         <div class="col text-center"><a style="text-decoration: none; color: white;" href="https://instagram.com/attitudesinmotiondancestudio" target="_blank"><ion-icon style="font-size: 22px; color: #EE1B4D;" name="logo-instagram"></ion-icon><br><span class="text-dark">Instagram</span></a></div>
